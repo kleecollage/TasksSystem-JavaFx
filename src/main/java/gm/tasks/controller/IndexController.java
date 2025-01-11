edefb62c0d38a6e1563c5c10c998092a4f4459fc
Klee Collage <klee.collage@gmail.com>
2025-01-11 15:30:11 -0600

    feat(JavaFx): Implements JavaFx

---
+++ src/main/java/gm/tasks/controller/IndexController.java
@@ -0,0 +1,13 @@
+package gm.tasks.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IndexController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/gm/tasks/presentation/TasksSystemFx.java
@@ -0,0 +1,16 @@
+package gm.tasks.presentation;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TasksSystemFx extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
